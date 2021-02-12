108edacc5906f41871a16aca3c75895dfe5b5579
Surabhi <surabhigeorge123@gmail.com>
2021-02-12 18:32:58 +0530

    Tab styling

--- src/app/ui-tab/ui-tab.component.ts
+++ src/app/ui-tab/ui-tab.component.ts
@@ -15,15 +15,15 @@ export class UiTabComponent implements OnInit {
   tabs = [
       {
           name: 'Tab 1',
-          data: 'Tab content 1'
+          data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book."
       },
       {
           name: 'Tab 2',
-          data: 'Tab content 2 updated'
+          data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book."
       },
       {
           name: 'Tab 3',
-          data: 'Tab content 3'
+          data: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book."
       }
   ]
 
